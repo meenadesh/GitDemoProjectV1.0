899343dbb3ade0b87170433893ada283bc1a8005
prafs <prafs@LAPTOP-PRAFULLA>
2020-05-12 19:45:45 +0530

    First Commit...

---
+++ src/main/java/com/gitdemoproject/testGitDemo.java
@@ -0,0 +1,19 @@
+package com.gitdemoproject;
+
+public class testGitDemo {
+
+	public static void main(String[] args) {
+
+		String Hungry = "yes";
+		
+		if (Hungry == "yes") {
+			System.out.println("Eat Something...");
+
+		} else {
+			System.out.println("Do your Work...");
+
+		}
+
+	}
+
+}
